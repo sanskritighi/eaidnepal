8cba048f54719a8d17505bff348ad880586c2368
Kanchan Sapkota <kanchansapkota27@gmail.com>
2024-03-13 23:07:05 -0400

    fix: update axios api

--- src/api/axios.js
+++ src/api/axios.js
@@ -5,5 +5,5 @@ const DEPLOY_URL=process.env.DEPLOY_URL || "http://napa.pythonanywhere.com/"
 
 export default axios.create({
   // baseURL: "https://napa.pythonanywhere.com/",
-  baseURL: process.env.IS_PRODUCTION==true?DEPLOY_URL:"http://localhost:8000/",
+  baseURL: process.env.IS_PRODUCTION==="true"?DEPLOY_URL:"http://localhost:8000/",
 });
